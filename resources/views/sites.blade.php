fa8f8e550c57648b60f0e25853d8681f72a8c7f7
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2022-07-05 18:29:21 +0200

    Settings

--- resources/views/sites.blade.php
+++ resources/views/sites.blade.php
@@ -1,10 +1,13 @@
 <x-app-layout>
-<x-slot name="header">
+    <x-slot name="header">
         <div class="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap">
             <div class="ml-4 mt-2">
                 <h3 class="py-2 text-2xl leading-6 font-medium text-gray-900 font-black">{{ __('Sites') }}</h3>
             </div>
             <div class="ml-4 mt-2 flex-shrink-0">
+                <button type="button" class="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    {{ __('Create') }}
+                </button>
                 <button type="button" class="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     {{ __('Create') }}
                 </button>
@@ -14,8 +17,28 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                TO DO
+            <div class="p-6 sm:px-20 bg-white">
+                <div class="py-2">
+                    <span class="h-12 w-auto">
+                        <div class="text-xl">
+                            Managed sites
+                        </div>
+                        <div class="text-3xl">
+                            <div style="cursor: pointer;">
+                                69
+                            </div>
+                        </div>
+                    </span>
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4">
+            <livewire:datatable
+                model="\App\Models\User"
+                name="users"
+                searchable="name, email"
+                include="id, name, email, created_at"
+                dates="created_at"
+            />
             </div>
         </div>
     </div>
