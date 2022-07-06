47399b3ac21d52101c641389bbac049c3ff0fbb9
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2022-07-06 16:58:04 +0200

    Datatable

--- routes/web.php
+++ routes/web.php
@@ -17,32 +17,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
     Route::get('/sites', function () {
         return view('sites');
     })->name('sites');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+    Route::get('/sites/index', [\App\Http\Controllers\SiteController::class, 'index']);
+    Route::get('/sites/create', [\App\Http\Controllers\SiteController::class, 'create']);
+    Route::post('/sites/create', [\App\Http\Controllers\SiteController::class, 'store']);
+    Route::get('/sites/edit/{id}', [\App\Http\Controllers\SiteController::class, 'show']);
+    Route::post('/sites/edit/{id}', [\App\Http\Controllers\SiteController::class, 'update']);
+    Route::get('/sites/delete/{id}', [\App\Http\Controllers\SiteController::class, 'destroy']);
+    
     Route::get('/settings', function () {
         return view('settings');
     })->name('settings');
+
 });
