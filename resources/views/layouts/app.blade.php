3c09e2fcc9a997a8d6e101368b0f9b1e7b121cb4
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2022-07-06 12:19:29 +0200

    Datatable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,49 @@
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
-        
-        
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" />
+        <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css" />
+
         @livewireStyles
         
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
         
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <script src="{{ mix('js/app.js') }}" defer></script>
+
+        <style>
+        .dataTables_length select {
+            padding-right: 2.5rem !important;
+        }
+        table.dataTable.dtr-inline.collapsed>tbody>tr>td.dtr-control, table.dataTable.dtr-inline.collapsed>tbody>tr>th.dtr-control {
+            position: relative;
+            padding-left: 30px;
+            cursor: pointer;
+        }
+        table.dataTable.dtr-inline.collapsed>tbody>tr.parent>td.dtr-control:before, table.dataTable.dtr-inline.collapsed>tbody>tr.parent>th.dtr-control:before {
+            content: "-";
+        }
+        table.dataTable.dtr-inline.collapsed>tbody>tr>td.dtr-control:before, table.dataTable.dtr-inline.collapsed>tbody>tr>th.dtr-control:before {
+            top: 33%;
+            left: 5px;
+            height: 1em;
+            width: 1em;
+            margin-top: -5px;
+            display: block;
+            position: absolute;
+            color: #fff;
+            box-sizing: content-box;
+            text-align: center;
+            text-indent: 0 !important;
+            font-family: monospace;
+            line-height: 1em;
+            content: "+";
+            background-color: #000;
+        }
+        </style>
+        
         
     </head>
     <body class="font-sans antialiased">
