87103ec54059b8e9a9e297fba956174891e85d85
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2022-07-09 22:40:52 +0200

    Fix

--- resources/views/sites/_submenu.blade.php
+++ resources/views/sites/_submenu.blade.php
@@ -5,11 +5,11 @@
             <option value="/sites/{{ $site }}/edit/settings" @if(Request::is('*/settings')) selected @endif>{{ __('Setting') }}</option>
             <option value="/sites/{{ $site }}/edit/aliases" @if(Request::is('*/aliases')) selected @endif>{{ __('Aliases') }}</option>
             <option value="/sites/{{ $site }}/edit/ssl" @if(Request::is('*/ssl')) selected @endif>{{ __('SSL') }}</option>
-            <option value="/sites/{{ $site }}/edit/deploy" @if(Request::is('*/deploy')) selected @endif>{{ __('Deploy') }}</option>
-            <option value="/sites/{{ $site }}/edit/queue" @if(Request::is('*/queue')) selected @endif>{{ __('Queue') }}</option>
             <option value="/sites/{{ $site }}/edit/enviroment" @if(Request::is('*/enviroment')) selected @endif>{{ __('Environment') }}</option>
             <option value="/sites/{{ $site }}/edit/packages" @if(Request::is('*/packages')) selected @endif>{{ __('Packages') }}</option>
+            <option value="/sites/{{ $site }}/edit/deploy" @if(Request::is('*/deploy')) selected @endif>{{ __('Deploy') }}</option>
             <option value="/sites/{{ $site }}/edit/nginx" @if(Request::is('*/nginx')) selected @endif>{{ __('Nginx') }}</option>
+            <option value="/sites/{{ $site }}/edit/queue" @if(Request::is('*/queue')) selected @endif>{{ __('Queue') }}</option>
             <option value="/sites/{{ $site }}/edit/security" @if(Request::is('*/security')) selected @endif>{{ __('Security') }}</option>
             <option value="/sites/{{ $site }}/edit/delete" @if(Request::is('*/delete')) selected @endif>{{ __('Delete') }}</option>
         </select>
@@ -47,22 +47,6 @@
                     @endif
                 > {{ __('SSL') }} </a>
 
-                <a href="/sites/{{ $site }}/edit/deploy"
-                    @if(Request::is('*/deploy'))
-                        class="border-indigo-500 text-indigo-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" aria-current="page"
-                    @else
-                        class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm"
-                    @endif
-                > {{ __('Deploy') }} </a>
-
-                <a href="/sites/{{ $site }}/edit/queue"
-                    @if(Request::is('*/queue'))
-                        class="border-indigo-500 text-indigo-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" aria-current="page"
-                    @else
-                        class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm"
-                    @endif
-                > {{ __('Queue') }} </a>
-
                 <a href="/sites/{{ $site }}/edit/enviroment"
                     @if(Request::is('*/enviroment'))
                         class="border-indigo-500 text-indigo-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" aria-current="page"
@@ -79,6 +63,15 @@
                     @endif
                 > {{ __('Packages') }} </a>
 
+
+                <a href="/sites/{{ $site }}/edit/deploy"
+                    @if(Request::is('*/deploy'))
+                        class="border-indigo-500 text-indigo-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" aria-current="page"
+                    @else
+                        class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm"
+                    @endif
+                > {{ __('Deploy') }} </a>
+
                 <a href="/sites/{{ $site }}/edit/nginx"
                     @if(Request::is('*/nginx'))
                         class="border-indigo-500 text-indigo-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" aria-current="page"
@@ -86,6 +79,14 @@
                         class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm"
                     @endif
                 > {{ __('Nginx') }} </a>
+
+                <a href="/sites/{{ $site }}/edit/queue"
+                    @if(Request::is('*/queue'))
+                        class="border-indigo-500 text-indigo-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm" aria-current="page"
+                    @else
+                        class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm"
+                    @endif
+                > {{ __('Queue') }} </a>
 
                 <a href="/sites/{{ $site }}/edit/security"
                     @if(Request::is('*/security'))
